1d7a810833e1731ad784897b8ade7f669876217b
onekhai <khaizuransevn@gmail.com>
2025-01-06 17:30:31 +0700

    Layouting profile

--- celestial-ticket/app/(main)/profile/index.tsx
+++ celestial-ticket/app/(main)/profile/index.tsx
@@ -1,12 +1,46 @@
 import { useNavigation } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
-import { Link } from "expo-router";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import Ticket from "../../components/Ticket";
+
 export default function ProfileScreen() {
-  const navigation = useNavigation();
-  return (
-    <View>
-      <Text>Profile Screen</Text>
-      <Link href="login">About</Link>
-    </View>
-  );
+    const user = [
+        {
+            id: 1,
+            name: "John Doe",
+            email: "johndoe@example.com",
+            password: "password123",
+            phoneNumber: "+1234567890",
+            address: "Jakarta, Indonesia",
+            gender: "male",
+            coordinates: {
+                latitude: 37.7749,
+                longitude: -122.4194,
+            },
+        },
+    ];
+
+    return (
+        <SafeAreaView className="flex">
+            <View className="flex items-center">
+                <Text className="text-center text-3xl font-bold mt-16">
+                    My Profile
+                </Text>
+                <FontAwesome6
+                    name="user-circle"
+                    size={100}
+                    color="black"
+                    style={{ marginTop: 7 }}
+                />
+                <Text className="text-center text-lg font-bold mt-5">
+                    {user[0].name}
+                </Text>
+                <Text className="text-center text-lg">{user[0].email}</Text>
+            </View>
+                <Text className="m-5 font-semibold">My Tickets</Text>
+            <View className="flex flex-row justify-center">
+                <Ticket />
+            </View>
+        </SafeAreaView>
+    );
 }
